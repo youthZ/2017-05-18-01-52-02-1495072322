e18300d3dbde58064354b6a4f17610d18dd9f453
youths <552099787@qq.com>
2017-05-23 16:01:17 +0800

    finish

--- main/person.js
+++ main/person.js
@@ -1,2 +1,12 @@
 // Write your code here
 module.exports = Person
+function Person(name,age){
+	this.name = name;
+	this.age = age;
+}
+Person.prototype={
+	constructor:Person,
+	introduce:function(){
+		return "My name is"+this.name+". "+"I am"+this.age+"years old.";
+	}
+}
--- main/student.js
+++ main/student.js
@@ -1,3 +1,18 @@
 let Person = require('./person')
 // Write your code here
 module.exports = Student
+function Student(name,age,klass){
+	person.call(this,name,age);
+	this.klass = klass;
+}
+Student.prototype = new Person();
+
+Studnet.prototype={
+	constructor:Student,
+	introduce:function(){
+		Person.call(Student);
+		
+		return Student.introduce()+"I am at Class"+this.klass+". ";
+	}
+	
+}
